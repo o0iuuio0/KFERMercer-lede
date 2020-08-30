aa85ba3ed7f0829b4c9b63c14665738ef19d761c
tcsr200722 <350524166@qq.com>
2020-08-30 08:38:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,24 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+add koolproxyR 1
+#git clone https://github.com/tcsr200722/luci-app-koolproxyR.git package/luci-app-koolproxyR
+add koolproxyR 2
+#git clone https://github.com/tcsr200722/ycg31-luci-app-koolproxyR.git package/luci-app-koolproxyR
+add luci-app-filebrowser
+git clone https://github.com/tcsr200722/FileBrowser.git package/luci-app-filebrowser
+add luci-app-adguardhome 1
+#git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+add luci-app-adguardhome 2
+#git clone https://github.com/tcsr200722/luci-app-adguardhome-new.git package/luci-app-adguardhome
+add luci-app-oaf
+#git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
+add luci-app-dockerman
+#git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+add luci-lib-docker
+#git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
+add luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+./scripts/feeds update -a
+./scripts/feeds install -a
